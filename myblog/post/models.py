82080d3aeee689048ae9108240c5c13999c05068
liuredflash <522903052@qq.com>
2018-12-23 14:55:44 +0800

    add post and comment model

--- myblog/post/models.py
+++ myblog/post/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+	uid = models.IntegerField()
+	title = models.CharField(max_length=64)
+	create = models.DateTimeField(auto_now_add=True)
+	content = models.TextField()
+
+
+class Comment(models.Model):
+	pid = models.IntegerField()
+	uid = models.IntegerField()
+	create = models.DateTimeField(auto_now_add=True)
+	content = models.TextField()
